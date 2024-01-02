52f18080321d7b2793e844b4039d2388d4171ff1
Elise Ngo <elisetvy@gmail.com>
2024-01-02 12:18:02 -0800

    Convert config file to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
+import { db } from "./firebase-config.ts";
+
 function App() {
 
+  const [users, setUsers] = useState([]);
+  const usersRef =
+
+  useEffect(() => {
+    const getUsers = () => {
+
+    }
+
+    getUsers();
+  }, []);
+
   return (
+
     <>
-      <h1 className="">Burner</h1>
+
     </>
   )
 }
