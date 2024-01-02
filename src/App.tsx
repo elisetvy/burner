a0f45275b281d8d41abfd8733cf4fc3daf5ad3a4
Elise Ngo <elisetvy@gmail.com>
2024-01-02 12:40:23 -0800

    Set up database query

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase-config.ts";
+
+interface User {
+  name: string
+}
 
 function App() {
 
   const [users, setUsers] = useState([]);
-  const usersRef =
+  const usersRef = collection(db, "users");
 
   useEffect(() => {
-    const getUsers = () => {
-
+    const getUsers = async () => {
+      const data = await getDocs(usersRef);
+      console.log(data)
+      setUsers(data.docs.map((doc:object) => ({ ...doc.data(), id: doc.id })));
     }
 
     getUsers();
   }, []);
 
   return (
-
     <>
-
+      { users.map((user:User) => {
+        <h1>{user.name}</h1>
+      })}
     </>
   )
 }
