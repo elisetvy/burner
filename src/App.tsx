19293705aa47cb4d653ac776a513cfcc04f12325
Elise Ngo <elisetvy@gmail.com>
2024-01-02 19:47:59 -0800

    Fetch random cat img

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
 import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase-config.ts";
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "./firebase-config.ts";
 
-interface User {
-  name: string
-}
+// interface User {
+//   name: string
+// }
 
 function App() {
 
-  const [users, setUsers] = useState([]);
-  const usersRef = collection(db, "users");
+  // const [users, setUsers] = useState([]);
+  // const usersRef = collection(db, "users");
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersRef);
+  //     console.log(data)
+  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   }
+
+  //   getUsers();
+  // }, []);
+
+  const [cat, setCat] = useState(null);
 
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersRef);
-      console.log(data)
-      setUsers(data.docs.map((doc:object) => ({ ...doc.data(), id: doc.id })));
+    const getCat = async () => {
+      const resp = await fetch("https://api.thecatapi.com/v1/images/search");
+      const data = await resp.json()
+
+      setCat(data[0].url);
     }
 
-    getUsers();
-  }, []);
+    getCat();
+  }, [])
 
   return (
     <>
-      { users.map((user:User) => {
+      {/* { users.map((user:User) => {
         <h1>{user.name}</h1>
-      })}
+      })} */}
+      <img src={cat} alt="cat" />
     </>
   )
 }
